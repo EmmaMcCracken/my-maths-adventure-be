6b052679ccd683b52ca82336d089e3eaa8276941
Emma McCracken <emma.mccracken14@gmail.com>
2022-01-26 16:01:37 +0000

    add get lessons by userid, and get numberoflessonparticipants by lessonid

--- server.ts
+++ server.ts
@@ -11,8 +11,8 @@ config(); //Read .env file lines as though they were env vars.
 //For the ssl property of the DB connection config, use a value of...
 // false - when connecting to a local DB
 // { rejectUnauthorized: false } - when connecting to a heroku DB
-const herokuSSLSetting = { rejectUnauthorized: false }
-const sslSetting = process.env.LOCAL ? false : herokuSSLSetting
+const herokuSSLSetting = { rejectUnauthorized: false };
+const sslSetting = process.env.LOCAL ? false : herokuSSLSetting;
 const dbConfig = {
   connectionString: process.env.DATABASE_URL,
   ssl: sslSetting,
@@ -21,30 +21,36 @@ const dbConfig = {
 const app = express();
 
 app.use(express.json()); //add body parser to each following route handler
-app.use(cors()) //add CORS support to each following route handler
+app.use(cors()); //add CORS support to each following route handler
 
 const client = new Client(dbConfig);
 client.connect();
 
 app.get("/", async (req, res) => {
-  res.json({ Message: "Welcome to the my-maths-adventure API.", Documentation: "link to API documentation will be given here" });
+  res.json({
+    Message: "Welcome to the my-maths-adventure API.",
+    Documentation: "link to API documentation will be given here",
+  });
 });
 
 app.get("/users", async (req, res) => {
-  try{const dbres = await client.query('select userid, username, preferredname, age, qualification from users');
-  res.status(200).json({message: "success", users: dbres.rows});}
-  catch(error){console.error(error)}
+  try {
+    const dbres = await client.query(
+      "select userid, username, preferredname, age, qualification from users"
+    );
+    res.status(200).json({ message: "success", users: dbres.rows });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.post("/users", async (req, res) => {
+  const { username, emailaddress, preferredname, age, qualification } =
+    req.body;
   try {
-    const { username, emailaddress, preferredname, age, qualification
-    } = req.body;
     const dbres = await client.query(
       "INSERT INTO users (username, emailaddress, preferredname, age, qualification) VALUES ($1,$2,$3,$4,$5) returning *",
-      [
-        username, emailaddress, preferredname, age, qualification
-      ]
+      [username, emailaddress, preferredname, age, qualification]
     );
     if (dbres.rows.length > 0) {
       res.status(200).json({
@@ -59,7 +65,6 @@ app.post("/users", async (req, res) => {
         message: "no user added",
       });
     }
-
   } catch (error) {
     console.error(error);
     if (error.code === 23505 || error.code === "23505") {
@@ -70,14 +75,38 @@ app.post("/users", async (req, res) => {
       });
     }
   }
-})
-  ;
+});
 
+app.get("/lessons/:userid", async (req, res) => {
+  const userid = req.params.userid;
+  try {
+    const dbres = await client.query(
+      "SELECT lessons.lessonid,time,title,description FROM lessons join lessonparticipants on lessons.lessonid=lessonparticipants.lessonid WHERE userid=$1",
+      [userid]
+    );
+    res.status(200).json({ message: "success", lessons: dbres.rows });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.get("/numberoflessonparticipants/:lessonid", async (req, res) => {
+  const lessonid = req.params.lessonid;
+  try {
+    const dbres = await client.query(
+      "SELECT count(*) FROM lessons JOIN lessonparticipants ON lessons.lessonid=lessonparticipants.lessonid WHERE lessons.lessonid=$1",
+      [lessonid]
+    );
+    res.status(200).json({ message: "success", count: dbres.rows[0].count });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 //Start the server on the given port
 const port = process.env.PORT;
 if (!port) {
-  throw 'Missing PORT environment variable.  Set it in .env file.';
+  throw "Missing PORT environment variable.  Set it in .env file.";
 }
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
