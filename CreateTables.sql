1530bdaca15359d9132978ca524cf85e56bfa2d0
Emma McCracken <emma.mccracken14@gmail.com>
2022-01-27 20:18:11 +0000

    add more inser intos

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,9 +1,18 @@
+
+
+
+
+
+
+
+
+
+
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS lessons CASCADE;
 DROP TABLE IF EXISTS lessonparticipants;
 DROP TABLE IF EXISTS lessonreflections;
 DROP TABLE IF EXISTS lessoncounts;
-
 
 CREATE TABLE users (
   userid SERIAL PRIMARY KEY,
@@ -40,23 +49,38 @@ CREATE TABLE lessonparticipants (
     
    );
 
-  CREATE TABLE lessoncounts AS
-  SELECT lessons.lessonid, title, description, time, COUNT(lessonparticipants.lessonid)::INT AS participants 
-  FROM lessons JOIN lessonparticipants
-  ON lessons.lessonid=lessonparticipants.lessonid
-  GROUP BY lessons.lessonid;
+INSERT INTO users(username,emailaddress,age,qualification,preferredname)
+VALUES ('MathematicsForever','ember.emma.emmer@gmail.com',25,'N/A', 'Emma');
 
--- INSERT INTO users(username,emailaddress,age,qualification,preferredname)
--- VALUES ('MathematicsForever','ember.emma.emmer@gmail.com',25,'N/A', 'Emma');
+INSERT INTO users(username,emailaddress,age,qualification,preferredname)
+VALUES ('MathMagician','megan@gmail.com',17,'N/A', 'Megan');
 
--- INSERT INTO lessons(time,title,description)
--- VALUES ('2022-01-26 18:00','First Lesson', 'We will complete a past paper together');
+INSERT INTO users(username,emailaddress,age,qualification,preferredname)
+VALUES ('Louis','louis@gmail.com',17,'N/A', 'Louis');
 
--- INSERT INTO lessonparticipants (lessonid,userid)
--- VALUES (1,2);
+INSERT INTO lessons(time,title,description)
+VALUES ('2022-01-26 18:00','First Lesson', 'We will complete a past paper together');
 
--- INSERT INTO lessonparticipants (lessonid,userid)
--- VALUES (1,2);
+INSERT INTO lessonparticipants (lessonid,userid)
+VALUES (1,2);
 
--- INSERT INTO lessonparticipants (lessonid,userid)
--- VALUES (1,1);
+INSERT INTO lessonparticipants (lessonid,userid)
+VALUES (1,1);
+
+INSERT INTO lessonparticipants (lessonid,userid)
+VALUES (1,3);
+
+
+-- SELECT count(*) FROM lessons JOIN lessonparticipants ON lessons.lessonid=lessonparticipants.lessonid WHERE lessons.lessonid=1 ;
+
+
+
+CREATE TABLE lessoncounts AS
+SELECT lessons.lessonid, title, description, time, COUNT(lessonparticipants.lessonid)::int AS participants 
+FROM lessons JOIN lessonparticipants
+ON lessons.lessonid=lessonparticipants.lessonid
+GROUP BY lessons.lessonid;
+
+
+
+select lessoncounts.lessonid,title,description,time,participants from lessoncounts join lessonparticipants on lessoncounts.lessonid=lessonparticipants.lessonid where userid=1;
